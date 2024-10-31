314b1157031f67edc00c8a797e743c06d1dc0d34
ShiinaNeet <72781583+ShiinaNeet@users.noreply.github.com>
2024-10-31 22:19:11 +0800

    Updates

--- database/migrations/2024_10_29_144715_create_likes_table.php
+++ database/migrations/2024_10_29_144715_create_likes_table.php
@@ -12,12 +12,10 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('likes', function (Blueprint $table) {
-            Schema::create('likes', function (Blueprint $table) {
-                $table->id();
-                $table->foreignId('user_id')->constrained()->onDelete('cascade');
-                $table->foreignId('post_id')->constrained()->onDelete('cascade');
-                $table->timestamps();
-            });
+            $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->foreignId('post_id')->constrained()->onDelete('cascade');
+            $table->timestamps();
         });
     }
 
